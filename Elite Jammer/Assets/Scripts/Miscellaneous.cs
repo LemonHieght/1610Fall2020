eb89ca61efe6566c41d30bffde950826b1ed8a81
Will lemmons <will.e.lemmons@gmail.com>
2020-08-28 14:18:54 -0800

    Setting up Variables

---
+++ Elite Jammer/Assets/Scripts/Miscellaneous.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miscellaneous : MonoBehaviour
+{
+    public string words;
+
+    public bool truth;
+
+    public float wholeNumbers;
+    
+    public int inbetween;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
