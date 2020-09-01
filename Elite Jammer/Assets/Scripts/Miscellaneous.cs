94a77474413a1c3dc4ee42741fffe789a5d6337b
Will lemmons <will.e.lemmons@gmail.com>
2020-09-01 13:40:55 -0800

    Mover Script

--- Elite Jammer/Assets/Scripts/Miscellaneous.cs
+++ Elite Jammer/Assets/Scripts/Miscellaneous.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,11 +18,18 @@ public class Miscellaneous : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debug.Log prints text in console
+        Debug.Log("");
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    
+    //Fixed Update is called once per physics update
+    private void FixedUpdate()
     {
         
     }
---
+++ Elite Jammer/Assets/Scripts/MoverScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    private float x, y, z;
+    public float speed;
+    private void Start()
+    {
+        Debug.Log("Wakey, Wakey you fool!");
+    }
+
+    private void Update()
+    {
+        Debug.Log("Turn off game.");
+        x = Input.GetAxis("Horizontal")* Time.deltaTime;
+        y = Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(speed * x,speed * y,z);
+    }
+}
