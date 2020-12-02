be8aa56f8d6dce3933c823aa3a647f93f60c58d5
Will lemmons <will.e.lemmons@gmail.com>
2020-12-02 14:27:02 -0900

    switch

---
+++ Elite Jammer/Assets/Scripts/Switchs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switchs : MonoBehaviour
+{
+    public int sickness= 4;
+
+    void Healthy()
+    {
+        switch (sickness)
+        {
+            case 4:
+                print("You're as healthy as an ox");
+                break;
+            case 3:
+                print("I guess you feel fine");
+                break;
+            case 2:
+                print("You look a little under the weather");
+                break;
+            case 1:
+                print("You look like walking death");
+                break;
+            default:
+                print("No sickness level detected");
+                break;
+        }
+    }
+}
