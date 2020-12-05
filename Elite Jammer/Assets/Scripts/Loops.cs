bcac2abc0539323c9e2a015ebef1b514ca3d0c27
Will lemmons <will.e.lemmons@gmail.com>
2020-12-04 16:51:14 -0900

    Loops

---
+++ Elite Jammer/Assets/Scripts/Loops.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int aliveEnemies;
+    public int numEnemies = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < numEnemies; x++)
+        {
+            Debug.Log("creating enemy number:" + x);
+        }
+        numEnemies = aliveEnemies;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (aliveEnemies <= 0)
+        {
+            Debug.Log("All enemies are Dead!");
+        }
+    }
+}
