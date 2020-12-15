b1eb81709f3d4a17bcac620db5b4c1da440d2797
Will lemmons <will.e.lemmons@gmail.com>
2020-12-14 18:59:38 -0900

    UI

---
+++ Elite Jammer/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public FloatScript healthBar;
+
+    public float health;
+
+    public bool alive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.value = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = healthBar.value;
+        if (health <= 0f)
+        {
+            FindObjectOfType<GameManger>().EndGame();
+        }
+    }
+}
